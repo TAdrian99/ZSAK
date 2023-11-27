c91a6cfd4ce23462c2796ca597431505c7b8faec
TAdrian99 <fonneszakon@gmail.com>
2023-11-27 19:43:56 +0100

    Menu Szerkezet by TAD

--- main.py
+++ main.py
@@ -12,7 +12,69 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
-print('Török')
 
 print('V1')
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+
+while True:
+    # Menü kiíratása
+    print("Válasszon egy lehetőséget:")
+    print("1. Új áru felvitele")
+    print("2. Listázás")
+    print("3. Elem törlése 3")
+    print("4. Felviteli részletek  4")
+    print("5. Legnehezebb áru 5")
+    print("6. Utoljára felvitt 6")
+    print("7. Kiszállítási dátum módosítása 7")
+    print("8. Futár cég felvitel 8")
+    print("9. Fuvarozó hozzárendelése 9")
+    print("0. Kilépés")
+
+    # Felhasználói input bekérése
+    valasztas = input("Kérem, válasszon (0-9): ")
+
+    # Kilépés ellenőrzése
+    if valasztas == '0':
+        print("Kilépés...")
+        break
+
+    # Választás kezelése
+    elif valasztas == '1':
+        print("Ön választotta az Opció 1-t.")
+        # Itt folytathatja az Opció 1-hoz tartozó műveleteket.
+
+    elif valasztas == '2':
+        print("Ön választotta az Opció 2-t.")
+        # Itt folytathatja az Opció 2-hoz tartozó műveleteket.
+
+    elif valasztas == '3':
+        print("Ön választotta az Opció 3-t.")
+        # Itt folytathatja az Opció 3-hoz tartozó műveleteket.
+
+    elif valasztas == '4':
+        print("Ön választotta az Opció 4-t.")
+        # Itt folytathatja az Opció 4-hoz tartozó műveleteket.
+
+    elif valasztas == '5':
+        print("Ön választotta az Opció 5-t.")
+        # Itt folytathatja az Opció 5-höz tartozó műveleteket.
+
+    elif valasztas == '6':
+        print("Ön választotta az Opció 6-t.")
+        # Itt folytathatja az Opció 6-hoz tartozó műveleteket.
+
+    elif valasztas == '7':
+        print("Ön választotta az Opció 7-t.")
+        # Itt folytathatja az Opció 7-hoz tartozó műveleteket.
+
+    elif valasztas == '8':
+        print("Ön választotta az Opció 8-t.")
+        # Itt folytathatja az Opció 8-hoz tartozó műveleteket.
+
+    elif valasztas == '9':
+        print("Ön választotta az Opció 9-t.")
+        # Itt folytathatja az Opció 9-hoz tartozó műveleteket.
+
+    else:
+        print("Érvénytelen választás. Kérem, válasszon újra.")

54cd5c97973ac694bc3a3e7da031a0cf1adc1689
Ed3tor <107465369+Ed3tor@users.noreply.github.com>
2023-11-30 22:36:19 +0100

    Új árú létrehozása
    new_product metódussal létrehozhatunk új árukat, amelyeknél a következő input lehetőségeket lehet megadni:
    
    név
    magasság
    szélesség
    ár
    forgalmazó
    
    Valamint ezeket az árukat egy aruk.txt fájlba menti és tárolja, valamint ellátja őket egy ID-val és egy rögzítési dátummal a nyomonkövethetőség miatt.

---
+++ new_product.py
@@ -0,0 +1,39 @@
+from datetime import datetime
+
+def new_product():
+    print("Ön az Új áru felvitele opciót választotta.")
+    nev = input("Adja meg az áru nevét: ")
+    magassag = input("Adja meg az áru magasságát(cm): ")
+    szelesseg = input("Adja meg az áru szélességét(cm): ")
+    ar = input("Adja meg az áru árát: ")
+    forgalmazo = input("Ki a forgalmazója az árunak?")
+
+    # Rögzítés ideje
+    now = datetime.now()
+    timestamp = now.strftime("%Y-%m-%d %H:%M:%S")
+
+    try:
+        # Az utolsó ID lekérése a fájlból
+        with open("aruk.txt", "r") as f:
+            lines = f.readlines()
+            last_id = 0
+
+            for line in reversed(lines):
+                if line.startswith("ID:"):
+                    last_id = int(line.split(":")[1].strip())
+                    break
+
+    except FileNotFoundError:
+        last_id = 0
+
+    # Új ID generálása
+    new_id = last_id + 1
+
+    # Az adatokat egy szövegfájlba írjuk
+    with open("aruk.txt", "a") as f:
+        f.write(f"\nID: {new_id}\nNév: {nev}\nMagasság: {magassag} cm\nSzélesség: {szelesseg} cm\nForgalmazó: {forgalmazo}\nÁr: {ar} Ft\nRögzítés ideje: {timestamp}\n")
+        f.write(f"-------------------------")
+
+    print("Az áru hozzáadva az árlistához. ID:", new_id)
+
+    input("Nyomja meg az entert a menübe való visszatéréshez...")

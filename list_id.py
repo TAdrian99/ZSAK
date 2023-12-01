3660b38040c3fea0410d9b933bb239652d557bfa
zsombormar <marzsomborbreznay2.0@gmail.com>
2023-12-01 11:31:29 +0100

    Létrehoztam a list_id fájl, ami tartalmaz egy metódust. Funkciója, hogy a felhasználó által megadott ID alapján kilistázza a hozzá tartozó adatokat. Ezt Felviteli részletek opciónak neveztük el. A main fájlba beimportáltam, a program működőképes. Hibakezelést is tartalmaz, többek között ha nincs a megadott ID-val rendelkező árucikk a txt-ben, érvénytelen bemenet, az aruk.txt fájl nem található.

---
+++ list_id.py
@@ -0,0 +1,26 @@
+def list_id():
+    try:
+        product_id = int(input("Adja meg az áru ID-jét a megjelenítéshez: "))
+
+        with open("aruk.txt", "r") as f:
+            lines = f.readlines()
+
+        found = False
+        for line in lines:
+            if line.startswith("ID:") and int(line.split(":")[1].strip()) == product_id:
+                found = True
+            elif found and line.startswith("-------------------------"):
+                found = False
+            elif found:
+                print(line.strip())
+
+        if not found:
+            print(f"Nincs találat az '{product_id}' ID-vel rendelkező termékre.")
+
+    except ValueError:
+        print("Érvénytelen bemenet. Kérlek, adj meg egy számot az áru ID-jéhez.")
+
+    except FileNotFoundError:
+        print("Az 'aruk.txt' fájl nem található.")
+
+    input("Nyomja meg az entert a menübe való visszatéréshez...")

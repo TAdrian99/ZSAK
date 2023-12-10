be1a921e9e73c812c9a7b0ad28b05aad780cf9e6
ocbtomi36 <118051723+ocbtomi36@users.noreply.github.com>
2023-12-10 09:05:46 +0100

    futarceg osztaly konstruktorának a kidolgozása

--- FutarCegFelvitel.py
+++ FutarCegFelvitel.py
@@ -1,9 +1,24 @@
-class Szemely:
+class Futarceg:
     def __init__(self, nev, cim, telefon):
-        self.nev = nev
-        self.cim = cim
-        self.telefon = telefon
-
+        if not nev or not isinstance(nev,str):
+            raise ValueError("A név nem lehet null vagy üres!")
+        else:
+            cleaned_nev = nev.strip().title()
+            self.nev = cleaned_nev
+        if not cim or not isinstance(cim,str):
+            raise ValueError("A cím nem lehet null vagy üres!")
+        else:
+            cleaned_cim = cim.strip().title()
+            self.cim = cleaned_cim
+        if not telefon or not isinstance(telefon,str):
+            raise ValueError("A telefon nem lehet null vagy üres!")
+        if not telefon.startswith("+"):
+            raise ValueError("Csak nemzetközi telefonszám adható meg, így a kezdőkarakter mindenféleképpen +")
+        if len(telefon) <= 8 or len(telefon) > 20:
+            raise ValueError("A telefonszám legalább 8 karakter hosszú vagy 20 karakternél rövidebbnek kell lennie!")
+        else:
+            cleaned_telefon = telefon.strip()
+            self.telefon = cleaned_telefon
     @property
     def nev(self):
         return self._nev
